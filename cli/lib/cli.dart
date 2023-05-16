a62f05e11575ddaa5e455221d748352ed5b598d9
yurygrivickiy <yury.grivickiy@finespace.com>
2023-05-16 22:39:12 +0300

    showMaxArrayValue

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,10 +1,16 @@
-int calculate() {
-  return 6 * 7;
-}
+import 'dart:math';
 
+class ArrayMax {
+  static showMaxArrayValue() {
+    List<int> values = [25, 5, 12, 44, 123, 1, 2];
+    int maxValue = 0;
+    for (int i = 0; i < values.length; i++) {
+      maxValue = max(maxValue, values[i]);
+    }
+    print(maxValue);
+  }
+}
 
 void main() {
-  print(calculate());
-
+  ArrayMax.showMaxArrayValue();
 }
-
