b53ea0e56e6ece3cfce3fb67054f9640aae6e788
yurygrivickiy <yury.grivickiy@finespace.com>
2023-06-10 23:02:50 +0300

    unique elements in array

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,10 +1,30 @@
-int calculate() {
-  return 6 * 7;
-}
+class ArrayMax {
+  static void main() {
+    final uniqueNumbers = [];
 
+    List<String> phoneNumbers = [
+      '+791610002030',
+      '+791612222030',
+      '+791610003122',
+      '+791610002030',
+    ];
+
+    for (var phoneNumber in phoneNumbers) {
+      bool alreadyExist = false;
+      for (var uniqueNumberItem in uniqueNumbers) {
+        if (uniqueNumberItem == phoneNumber) {
+          alreadyExist = true;
+          break;
+        }
+      }
+      if (alreadyExist == false) {
+        uniqueNumbers.add(phoneNumber);
+      }
+    }
+    print(uniqueNumbers);
+  }
+}
 
 void main() {
-  print(calculate());
-
+  ArrayMax.main();
 }
-
